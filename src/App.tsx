31e5d139380580d803c2a31411b3c878e13d1410
[dyad] <tensiuneadm@gmail.com>
2026-01-21 15:05:31 -0300

    [dyad] wrote 2 file(s), deleted 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index"; // This will be replaced by HomePage for authenticated users
 import NotFound from "./pages/NotFound";
 import LoginPage from "./pages/auth/LoginPage";
 import RegisterPage from "./pages/auth/RegisterPage";
