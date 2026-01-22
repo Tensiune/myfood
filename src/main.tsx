b2f30c26a5a0e78f14117ebee81f84ce07ee0a9a
[dyad] <tensiuneadm@gmail.com>
2026-01-22 16:16:14 -0300

    [dyad] Refatorando ponto de entrada para resolver erro de renderização - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 import { NotificationProvider } from "./context/NotificationContext";
 
 const rootElement = document.getElementById("root");
 
-if (!rootElement) {
-  throw new Error("Não foi possível encontrar o elemento root no documento.");
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <NotificationProvider>
+        <App />
+      </NotificationProvider>
+    </React.StrictMode>
+  );
 }
-
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
-  <React.StrictMode>
-    <NotificationProvider>
-      <App />
-    </NotificationProvider>
-  </React.StrictMode>
-);
