5f1fe22d95ebfb058d366843d422cb1e8ba243ac
[dyad] <tensiuneadm@gmail.com>
2026-01-22 16:15:50 -0300

    [dyad] Corrigindo erro de renderização no ponto de entrada do app - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 import { NotificationProvider } from "./context/NotificationContext";
 
-createRoot(document.getElementById("root")!).render(
-  <NotificationProvider>
-    <App />
-  </NotificationProvider>
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Não foi possível encontrar o elemento root no documento.");
+}
+
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <NotificationProvider>
+      <App />
+    </NotificationProvider>
+  </React.StrictMode>
 );
